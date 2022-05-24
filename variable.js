ec0957ccd0a9bb8d5257fd244e5ce6184dbe3b80
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-24 11:37:14 +0530

    variable end

--- variable.js
+++ variable.js
@@ -7,3 +7,17 @@ console.log(num + 2);
 
 let username = "Angad'd department";
 console.log(username);
+
+var number = 007; // global variable.
+            
+function someFunc() {
+    document.writeln(number);
+}
+// calling the function
+someFunc();
+/* 
+    trying to access the 
+    global variable
+*/
+document.writeln("<br>");
+document.writeln(number);
