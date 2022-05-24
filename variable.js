37bf7230af871db1805460e4101629e13da41918
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-24 12:47:04 +0530

    using const

--- variable.js
+++ variable.js
@@ -8,16 +8,9 @@ console.log(num + 2);
 let username = "Angad'd department";
 console.log(username);
 
-var number = 007; // global variable.
-            
-function someFunc() {
-    document.writeln(number);
-}
-// calling the function
-someFunc();
-/* 
-    trying to access the 
-    global variable
-*/
-document.writeln("<br>");
-document.writeln(number);
+let radius = 5
+const  pi = 3.14
+let area
+area = pi * radius *radius
+
+console.log(area)
